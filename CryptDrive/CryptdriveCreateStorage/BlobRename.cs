a9d8fac7faf38b00945735a4a05fc05bd50e41fe
SebastianHerr <Sebastian-Herr@web.de>
2018-09-14 17:58:02 +0200

    Added Exception handling to BlobRename so that we can get better error message from the runtime

--- CryptDrive/CryptdriveCreateStorage/BlobRename.cs
+++ CryptDrive/CryptdriveCreateStorage/BlobRename.cs
@@ -18,84 +18,94 @@ namespace CryptdriveCloud
         [FunctionName("BlobRename")]
         public static async System.Threading.Tasks.Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string containerName = req.Query["container"];
-            if (containerName == null)
-            {
-                string errorMessage = "No container name given!";
-                log.LogError(errorMessage);
-                return new BadRequestObjectResult(errorMessage);
-            }
-
-            string filenameOld = req.Query["filenameOld"];
-            if (filenameOld == null)
-            {
-                string errorMessage = "No old file name given!";
-                log.LogError(errorMessage);
-                return new BadRequestObjectResult(errorMessage);
-            }
-
-            string filenameNew = req.Query["filenameNew"];
-            if (filenameNew == null)
-            {
-                string errorMessage = "No new file name given!";
-                log.LogError(errorMessage);
-                return new BadRequestObjectResult(errorMessage);
-            }
-
-            log.LogInformation("Got the names:" + filenameOld + "->" + filenameNew + " in " + containerName);
-            CloudBlobContainer container = BlobManager.getBlobContainer(containerName);
-
-            // Based on https://github.com/Azure-Samples/storage-blob-dotnet-getting-started/blob/master/BlobStorage/Advanced.cs function CopyBlockBlobAsync
-            var sourceBlob = container.GetBlockBlobReference(filenameOld); ;
-            var destBlob = container.GetBlockBlobReference(filenameNew);
-            string leaseId = null;
-            log.LogInformation("Got the container references");
-
             try
             {
-                // Lease the source blob for the copy operation to prevent another client from modifying it.
-                // Specifying null for the lease interval creates an infinite lease.
-                leaseId = await sourceBlob.AcquireLeaseAsync(null);
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                log.LogInformation("Request is a " + System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-                // Ensure that the source blob exists.
-                if (await sourceBlob.ExistsAsync())
+                string containerName = req.Query["container"];
+                if (containerName == null)
                 {
-                    // Get the ID of the copy operation.
-                    string copyId = await destBlob.StartCopyAsync(sourceBlob);
+                    string errorMessage = "No container name given!";
+                    log.LogError(errorMessage);
+                    return new BadRequestObjectResult(errorMessage);
+                }
 
-                    // Fetch the destination blob's properties before checking the copy state.
-                    await destBlob.FetchAttributesAsync();
+                string filenameOld = req.Query["filenameOld"];
+                if (filenameOld == null)
+                {
+                    string errorMessage = "No old file name given!";
+                    log.LogError(errorMessage);
+                    return new BadRequestObjectResult(errorMessage);
+                }
 
-                    log.LogInformation("Status of copy operation: {0}", destBlob.CopyState.Status);
-                    log.LogInformation("Completion time: {0}", destBlob.CopyState.CompletionTime);
-                    log.LogInformation("Bytes copied: {0}", destBlob.CopyState.BytesCopied.ToString());
-                    log.LogInformation("Total bytes: {0}", destBlob.CopyState.TotalBytes.ToString());
+                string filenameNew = req.Query["filenameNew"];
+                if (filenameNew == null)
+                {
+                    string errorMessage = "No new file name given!";
+                    log.LogError(errorMessage);
+                    return new BadRequestObjectResult(errorMessage);
+                }
 
+                log.LogInformation("Got the names:" + filenameOld + "->" + filenameNew + " in " + containerName);
+                CloudBlobContainer container = BlobManager.getBlobContainer(containerName);
+
+                // Based on https://github.com/Azure-Samples/storage-blob-dotnet-getting-started/blob/master/BlobStorage/Advanced.cs function CopyBlockBlobAsync
+                var sourceBlob = container.GetBlockBlobReference(filenameOld); ;
+                var destBlob = container.GetBlockBlobReference(filenameNew);
+                string leaseId = null;
+                log.LogInformation("Got the container references");
+
+                try
+                {
+                    // Lease the source blob for the copy operation to prevent another client from modifying it.
+                    // Specifying null for the lease interval creates an infinite lease.
+                    leaseId = await sourceBlob.AcquireLeaseAsync(null);
+
+                    // Ensure that the source blob exists.
+                    if (await sourceBlob.ExistsAsync())
+                    {
+                        // Get the ID of the copy operation.
+                        string copyId = await destBlob.StartCopyAsync(sourceBlob);
+
+                        // Fetch the destination blob's properties before checking the copy state.
+                        await destBlob.FetchAttributesAsync();
+
+                        log.LogInformation("Status of copy operation: {0}", destBlob.CopyState.Status);
+                        log.LogInformation("Completion time: {0}", destBlob.CopyState.CompletionTime);
+                        log.LogInformation("Bytes copied: {0}", destBlob.CopyState.BytesCopied.ToString());
+                        log.LogInformation("Total bytes: {0}", destBlob.CopyState.TotalBytes.ToString());
+
+                        await releaseLease(sourceBlob);
+
+                        log.LogInformation("Released the lease, now comes the deleting...");
+
+                        //delete old file after copy is done
+                        await sourceBlob.DeleteIfExistsAsync();
+                        log.LogInformation("... file deleted, rename finished");
+                    }
+                    else
+                    {
+                        log.LogError("Source not found!");
+                    }
+                }
+                catch (StorageException e)
+                {
+                    log.LogError(e.Message);
+                    throw;
+                }
+                finally
+                {
                     await releaseLease(sourceBlob);
-
-                    log.LogInformation("Released the lease, now comes the deleting...");
-
-                    //delete old file after copy is done
-                    await sourceBlob.DeleteIfExistsAsync();
-                    log.LogInformation("... file deleted, rename finished");
                 }
-                else
-                {
-                    log.LogError("Source not found!");
-                }
+                return (ActionResult)new OkObjectResult($"Hello, ");
             }
-            catch (StorageException e)
+            catch (Exception e)
             {
                 log.LogError(e.Message);
-                throw;
+                log.LogError(e.StackTrace);
+                return new UnprocessableEntityObjectResult(e.Message);
             }
-            finally
-            {
-                await releaseLease(sourceBlob);
-            }
-            return (ActionResult)new OkObjectResult($"Hello, ");
         }
 
         private static async Task releaseLease(CloudBlockBlob sourceBlob)
